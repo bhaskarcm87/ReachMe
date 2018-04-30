1e7ceba640ef3bc1937683389b5f8a5595228cf5
Sachin Kumar Patra <sachinkumarpatra@Kirusas-MacBook-Pro-2.local>
2018-04-30 13:07:51 +0530

    Profilescreen

---
+++ ReachMe/Extentions/UIView+Extensions.swift
@@ -0,0 +1,180 @@
+//
+//  UIView+Extensions.swift
+//  ReachMe
+//
+//  Created by Sachin Kumar Patra on 4/29/18.
+//  Copyright © 2018 sachin. All rights reserved.
+//
+
+extension UIView {
+    
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var customBorderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var customBorderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+    
+    @IBInspectable
+    public var maskToBounds: Bool {
+        get {
+            return layer.masksToBounds
+        }
+        set {
+            layer.masksToBounds = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowColor: UIColor? {
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.shadowColor = color.cgColor
+            } else {
+                layer.shadowColor = nil
+            }
+        }
+    }
+    
+    @IBInspectable
+    public var circleCorner: Bool {
+        get {
+            return min(bounds.size.height, bounds.size.width) / 2 == cornerRadius
+        }
+        set {
+            cornerRadius = newValue ? min(bounds.size.height, bounds.size.width) / 2 : cornerRadius
+        }
+    }
+    
+    @IBInspectable
+    public var shadowPath: CGPath? {
+        get {
+            return layer.shadowPath
+        }
+        set {
+            layer.shadowPath = newValue
+        }
+    }
+    
+    @IBInspectable
+    public var shadowShouldRasterize: Bool {
+        get {
+            return layer.shouldRasterize
+        }
+        set {
+            layer.shouldRasterize = newValue
+        }
+    }
+    
+    @IBInspectable
+    public var shadowRasterizationScale: CGFloat {
+        get {
+            return layer.rasterizationScale
+        }
+        set {
+            layer.rasterizationScale = newValue
+        }
+    }
+}
+
+// MARK: - Properties
+
+public extension UIView {
+    
+    /// Size of view.
+    public var size: CGSize {
+        get {
+            return self.frame.size
+        }
+        set {
+            self.width = newValue.width
+            self.height = newValue.height
+        }
+    }
+    
+    /// Width of view.
+    public var width: CGFloat {
+        get {
+            return self.frame.size.width
+        }
+        set {
+            self.frame.size.width = newValue
+        }
+    }
+    
+    /// Height of view.
+    public var height: CGFloat {
+        get {
+            return self.frame.size.height
+        }
+        set {
+            self.frame.size.height = newValue
+        }
+    }
+}
